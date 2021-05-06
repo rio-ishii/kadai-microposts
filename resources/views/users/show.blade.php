70040a4b58c3fd0af03317b59a37be782bf7a449
rio.ishii <study.it.git@gmail.com>
2021-05-06 06:32:50 +0000

    follow, unfollow

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -12,10 +12,14 @@
                     <img class="rounded img-fluid" src="{{ Gravatar::get($user->email, ['size' => 500]) }}" alt="">
                 </div>
             </div>
+            {{-- ユーザ情報 --}}
+            @include('users.card')
+            {{-- フォロー／アンフォローボタン --}}
+            @include('user_follow.follow_button')
         </aside>
         <div class="col-sm-8">
             <ul class="nav nav-tabs nav-justified mb-3">
-               {{-- ユーザ詳細タブ --}}
+                {{-- ユーザ詳細タブ --}}
                 <li class="nav-item">
                     <a href="{{ route('users.show', ['user' => $user->id]) }}" class="nav-link {{ Request::routeIs('users.show') ? 'active' : '' }}">
                         TimeLine
@@ -26,6 +30,8 @@
                 <li class="nav-item"><a href="#" class="nav-link">Followings</a></li>
                 {{-- フォロワー一覧タブ --}}
                 <li class="nav-item"><a href="#" class="nav-link">Followers</a></li>
+                {{-- タブ --}}
+                @include('users.navtabs')
             </ul>
             @if (Auth::id() == $user->id)
                 {{-- 投稿フォーム --}}
